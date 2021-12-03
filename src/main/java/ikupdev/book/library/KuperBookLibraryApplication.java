81991a62fb5f784d815292df0feab24531dbafa4
Ilya Kupriyanov <ikupdev@gmail.com>
2021-12-03 18:28:48 +0300

    app backbone

---
+++ src/main/java/ikupdev/book/library/KuperBookLibraryApplication.java
@@ -0,0 +1,13 @@
+package ikupdev.book.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuperBookLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuperBookLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ikupdev/book/library/KuperBookLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ikupdev.book.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuperBookLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
