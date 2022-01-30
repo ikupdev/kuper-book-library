ee85d379d98cb00ec27d74ab6144fc54a0ff779b
Ilya Kupriyanov <ikupdev@gmail.com>
2022-01-30 15:24:41 +0300

    SpringFoxConfig tuned

--- src/main/java/ru/ikupdev/library/config/SpringFoxConfig.java
+++ src/main/java/ru/ikupdev/library/config/SpringFoxConfig.java
@@ -1,16 +1,25 @@
 package ru.ikupdev.library.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import static springfox.documentation.spring.web.paths.Paths.removeAdjacentForwardSlashes;
 
 /**
  * @author Ilya V. Kupriyanov
@@ -19,24 +28,42 @@ import java.util.Collections;
 @Configuration
 @EnableSwagger2
 public class SpringFoxConfig {
+    private static final ResourceBundle BUNDLE = ResourceBundle.getBundle(SpringFoxConfig.class.getName());
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        Docket docket = new Docket(DocumentationType.SWAGGER_2);
+        List<ResponseMessage> responseMessages = Arrays.asList(
+                new ResponseMessageBuilder().code(400).message(BUNDLE.getString("swagger.400.message")).build(),
+                new ResponseMessageBuilder().code(403).message(BUNDLE.getString("swagger.403.message")).build(),
+                new ResponseMessageBuilder().code(404).message(BUNDLE.getString("swagger.404.message")).build(),
+                new ResponseMessageBuilder().code(409).message(BUNDLE.getString("swagger.409.message")).build(),
+                new ResponseMessageBuilder().code(500).message(BUNDLE.getString("swagger.500.message")).build());
+        docket
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(RequestMethod.GET, responseMessages)
+                .globalResponseMessage(RequestMethod.POST, responseMessages)
+                .globalResponseMessage(RequestMethod.DELETE, responseMessages)
+                .globalResponseMessage(RequestMethod.PATCH, responseMessages)
+                .globalResponseMessage(RequestMethod.PUT, responseMessages)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("ru.ikupdev.library.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(this.apiInfo());
+        return docket;
     }
 
     private ApiInfo apiInfo() {
-        return new ApiInfo(
-                "Service Library",
-                "Description",
-                "1.0",
-                "Terms of service",
-                new Contact("Ilya Kupriyanov", null, "ikupdev@gmail.com"),
-                "License of API", "API license URL", Collections.emptyList());
+        return new ApiInfoBuilder()
+                .title("Service Library")
+                .description("Application for management of books")
+                .version("1.0.0")
+                .contact(new Contact(
+                        "Ilya Kupriyanov",
+                        null,
+                        "ikupdev@gmail.com"))
+                .build();
     }
 
 }
